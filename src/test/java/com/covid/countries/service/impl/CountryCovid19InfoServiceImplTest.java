a492cf44224a4048da4158b174b30a99c51fb0bd
AsenTerziyski <asenterziyski@gmail.com>
2022-02-02 12:47:32 +0200

    adding tests

--- src/test/java/com/covid/countries/service/impl/CountryCovid19InfoServiceImplTest.java
+++ src/test/java/com/covid/countries/service/impl/CountryCovid19InfoServiceImplTest.java
@@ -23,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CountryCovid19InfoServiceImplTest {
 
     private CountryCovid19InfoServiceImpl countryCovid19InfoServiceToTest;
+
     @Mock
     private CountriesCovidInfoRepository mockedRepo;
 
@@ -30,10 +31,18 @@ class CountryCovid19InfoServiceImplTest {
     private CountryCovidInfo country2;
     private CountryCovidInfo country3;
 
+    private static final String testInput = "{ID=3bbe3c8e-1e48-46a7-837d-49b0031d76d7, Message=, Global={NewConfirmed=3368438, TotalConfirmed=378639918, NewDeaths=8957, TotalDeaths=5672772, NewRecovered=0, TotalRecovered=0, Date=2022-02-02T06:59:35.139Z}, Countries=[{ID=1a3ab171-22f9-41e4-9da3-c07c1091ef81x, Country=TestXX, CountryCode=ZZ, Slug=testx, NewConfirmed=1, TotalConfirmed=1, NewDeaths=1, TotalDeaths=1, NewRecovered=1, TotalRecovered=1, Date=2022-02-02T06:59:35.139Z, Premium={}}, {ID=1a3ab171-22f9-41e4-9da3-c07c1091ef81xxx, Country=TestZZ, CountryCode=XX, Slug=testz, NewConfirmed=2, TotalConfirmed=2, NewDeaths=2, TotalDeaths=2, NewRecovered=2, TotalRecovered=2, Date=2022-02-02T06:59:35.139Z, Premium={}}], Date=2022-02-02T06:59:35.139Z}";
+
     @BeforeEach
     void initCountries() {
-        this.country1 = new CountryCovidInfo().setCountryCode("ZZ");
-        this.country2 = new CountryCovidInfo().setCountryCode("XX");
+        this.country1 = new CountryCovidInfo().setCountryCode("ZZ")
+                .setNewConfirmed("1")
+                .setNewRecovered("1")
+                .setNewDeaths("1");
+        this.country2 = new CountryCovidInfo().setCountryCode("XX")
+                .setNewConfirmed("2")
+                .setNewRecovered("2")
+                .setNewDeaths("2");
         this.country3 = new CountryCovidInfo().setCountryCode("NN");
     }
 
@@ -45,24 +54,31 @@ class CountryCovid19InfoServiceImplTest {
 
     @Test
     void processCovid19Data() {
-        String testInput = "{ID=3bbe3c8e-1e48-46a7-837d-49b0031d76d7, Message=, Global={NewConfirmed=3368438, TotalConfirmed=378639918, NewDeaths=8957, TotalDeaths=5672772, NewRecovered=0, TotalRecovered=0, Date=2022-02-02T06:59:35.139Z}, Countries=[{ID=1a3ab171-22f9-41e4-9da3-c07c1091ef81x, Country=TestXX, CountryCode=XX, Slug=testx, NewConfirmed=1, TotalConfirmed=1, NewDeaths=1, TotalDeaths=1, NewRecovered=1, TotalRecovered=1, Date=2022-02-02T06:59:35.139Z, Premium={}}, {ID=1a3ab171-22f9-41e4-9da3-c07c1091ef81xxx, Country=TestZZ, CountryCode=ZZ, Slug=testz, NewConfirmed=2, TotalConfirmed=2, NewDeaths=2, TotalDeaths=2, NewRecovered=2, TotalRecovered=2, Date=2022-02-02T06:59:35.139Z, Premium={}}], Date=2022-02-02T06:59:35.139Z}";
         List<CountryCovidInfo> countryCovidInfos = this.countryCovid19InfoServiceToTest.processCovid19Data(testInput);
         assertEquals(2, countryCovidInfos.size());
-        assertEquals("XX", countryCovidInfos.get(0).getCountryCode());
-        assertEquals("ZZ", countryCovidInfos.get(1).getCountryCode());
+
+        assertEquals("ZZ", countryCovidInfos.get(0).getCountryCode());
+        assertEquals("1", countryCovidInfos.get(0).getNewConfirmed());
+        assertEquals("1", countryCovidInfos.get(0).getNewDeaths());
+        assertEquals("1", countryCovidInfos.get(0).getNewRecovered());
+
+        assertEquals("XX", countryCovidInfos.get(1).getCountryCode());
+        assertEquals("2", countryCovidInfos.get(1).getNewConfirmed());
+        assertEquals("2", countryCovidInfos.get(1).getNewDeaths());
+        assertEquals("2", countryCovidInfos.get(1).getNewRecovered());
     }
 
     @Test
     void saveProcessedInputInfoInDb() {
-        Mockito.when(this.mockedRepo.findAll()).thenReturn(List.of(country1,country2));
-        List<CountryCovidInfo> countryCovidInfos = this.countryCovid19InfoServiceToTest.saveProcessedInputInfoInDb(List.of(country1, country2));
-        List<CountryCovidInfo> all = this.mockedRepo.findAll();
-        assertEquals(countryCovidInfos.get(0), all.get(0));
-        assertEquals(countryCovidInfos.get(1), all.get(1));
+        Mockito.when(this.mockedRepo.findAll()).thenReturn(List.of(country1, country2));
+        List<CountryCovidInfo> countryCovidInfos = this.countryCovid19InfoServiceToTest.processCovid19Data(testInput);
+        List<CountryCovidInfo> allCountriesInMockedRepo = this.mockedRepo.findAll();
+        assertEquals(countryCovidInfos.get(0).getCountryCode(), allCountriesInMockedRepo.get(0).getCountryCode());
+        assertEquals(countryCovidInfos.get(1).getCountryCode(), allCountriesInMockedRepo.get(1).getCountryCode());
         assertEquals(2L, countryCovidInfos.size());
-        Mockito.when(this.mockedRepo.findAll()).thenReturn(List.of(country1,country2,country3));
+        Mockito.when(this.mockedRepo.findAll()).thenReturn(List.of(country1, country2, country3));
         List<CountryCovidInfo> countryCovidInfos2 = this.countryCovid19InfoServiceToTest.saveProcessedInputInfoInDb(List.of(country1, country2, country3));
-        assertNotNull(country3);
+        assertEquals("NN", countryCovidInfos2.get(2).getCountryCode());
     }
 
     @Test
